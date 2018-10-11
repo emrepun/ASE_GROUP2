2c766102e5b613122db1eacd1ed4d19d7631854e
emrepun <emrehavan@hotmail.com>
2018-10-11 18:20:05 +0100

    first iOS Commit
    Empty Project set.

---
+++ ASE_iOS/ASE_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASE_iOS
+//
+//  Created by Emre HAVAN on 11.10.2018.
+//  Copyright © 2018 Emre HAVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
