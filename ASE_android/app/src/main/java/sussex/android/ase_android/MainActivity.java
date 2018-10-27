90fb8fb72f39e891d1d6036c3893676a829a41ee
ayyleesh <xxserpentxbizzarexx@yahoo.co.id>
2018-10-27 22:09:20 +0100

    updated drawables behaviors

--- ASE_android/app/src/main/java/sussex/android/ase_android/MainActivity.java
+++ ASE_android/app/src/main/java/sussex/android/ase_android/MainActivity.java
@@ -1,13 +1,36 @@
 package sussex.android.ase_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gotoLogin, gotoSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gotoLogin = (Button)findViewById(R.id.gotoLogin);
+        gotoSignup = (Button)findViewById(R.id.gotoSignup);
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gotoSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
