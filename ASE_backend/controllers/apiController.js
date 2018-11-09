99c467bf4549364e53c11f373b3c6d8f944e9a12
Rohan Bhayana <rallade@hotmail.com>
2018-11-09 15:02:27 +0000

    Basic endpoints with API integration with: doogal for postcode data the Government API for price

--- ASE_backend/controllers/apiController.js
+++ ASE_backend/controllers/apiController.js
@@ -4,3 +4,8 @@ exports.print_data = function(req, res) {
   apiService.print_data(req.body);
   res.redirect(200, '/');
 }
+
+exports.getPricesAt = async function(req, res) {
+  prices = await apiService.getPricesAt(req.params.lat, req.params.long);
+  res.send(JSON.stringify(prices, null, 2));
+}
