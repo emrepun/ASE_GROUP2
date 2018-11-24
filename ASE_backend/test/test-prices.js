608615c76ae8a66902c010a4c7faee848cae1809
TripleM <TripleM@TripleMs-MacBook-Air.local>
2018-11-24 12:51:55 +0000

    test works

--- ASE_backend/test/test-prices.js
+++ ASE_backend/test/test-prices.js
@@ -1,38 +1,14 @@
-
 const priceService =  require('../services/priceService.js');
 var nock = require('nock');
 var expect  = require('chai').expect;
 var request = require('request');
 
-
 describe('Average Prices', function() {
-  beforeEach(() => {
-  nock('http://landregistry.data.gov.uk/')
-  .get('data/ppi/transaction-record.json')
-  .reply(200,{
-    "format": "linked-data-api",
-    "version": "0.2",
-    "result": {
-      "items": [
-        {
-          "pricePaid":69,
-          "propertyAddress":"here and there",
-          "transactionDate":"about now"
-        },
-        {
-          "pricePaid":100,
-          "propertyAddress":"here and there",
-          "transactionDate":"about now"
-        }]}
-      });
-        });
-    it('calculates average prices per postcode', function(done) {
-      // Increase the default timeout for this test
-      // If the test takes longer than this, it will fail
 
-      return priceService.getAverageAtPostcode('BN1 7JJ')
-      .then(response => {
-      expect(avg==84.5);
+    it('calculates average prices per postcode', function() {
+        priceService.getAverageAtPostcode('BN1 7JJ').then(avg => {
+            expect(avg).to.equal('157661.39')
+        });
+
     });
   });
-});
