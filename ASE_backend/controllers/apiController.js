daa243968ae3e017a06e950f559500d01f82da0f
Rohan Bhayana <rallade@hotmail.com>
2018-11-24 15:06:08 +0000

    Added error handling with approprite http requests (and comments) to routes and controller

--- ASE_backend/controllers/apiController.js
+++ ASE_backend/controllers/apiController.js
@@ -1,3 +1,11 @@
+/**
+ * Abstracts the http request away from the function itself
+ * Simply passes the data to the appriate function
+ * Returns the appropriate HTTP response
+ * Is called by apiRoutes
+ * See apiRoutes for what each method actually does
+ */
+
 var apiService = require("../services/apiService");
 
 exports.print_data = function(req, res) {
@@ -5,26 +13,101 @@ exports.print_data = function(req, res) {
     res.redirect(200, "/");
 };
 
+/**
+ * Returns 400 if arguments are missing
+ * Returns 500 if there was an error processing the request
+ * @param {Express request} req
+ * @param {Express response} res
+ */
 exports.getPricesAtAround = async function(req, res) {
-    prices = await apiService.getPricesAtAround(
-        req.params.lat,
-        req.params.long,
-        req.params.radius
-    );
-    res.send(JSON.stringify(prices, null, 2));
+    if (req.params.lat == undefined || req.params.long == undefined) {
+        res.status(400);
+        res.send("Lat or long arguments missing");
+    } else {
+        try {
+            var prices = await apiService.getPricesAtAround(
+                req.params.lat,
+                req.params.long,
+                req.params.radius
+            );
+            res.send(JSON.stringify(prices, null, 2));
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(err);
+            res.status(500);
+            res.send("Error getting prices");
+        }
+    }
 };
 
+/**
+ * Returns 400 if arguments are missing
+ * Returns 500 if there was an error processing the request
+ * @param {Express request} req
+ * @param {Express response} res
+ */
 exports.getPostcodeData = async function(req, res) {
-    data = await apiService.getPostcodeData(req.params.postcode);
-    res.send(JSON.stringify(data, null, 2));
+    if (req.params.postcode == undefined) {
+        res.status(400);
+        res.send("Postcode argument missing");
+    } else {
+        try {
+            var data = await apiService.getPostcodeData(req.params.postcode);
+            res.send(JSON.stringify(data, null, 2));
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(err);
+            res.status(500);
+            res.send("Error getting addresses");
+        }
+    }
 };
 
+/**
+ * Returns 400 if arguments are missing
+ * Returns 500 if there was an error processing the request
+ * @param {Express request} req
+ * @param {Express response} res
+ */
 exports.getAveragePrice = async function(req, res) {
-    data = await apiService.getAveragePrice(req.params.postcode);
-    res.send(JSON.stringify(data, null, 2));
+    if (req.params.postcode == undefined) {
+        res.status(400);
+        res.send("Postcode argument missing");
+    } else {
+        try {
+            var data = await apiService.getAveragePrice(req.params.postcode);
+            res.send(JSON.stringify(data, null, 2));
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(err);
+            res.status(500);
+            res.send("Error getting average");
+        }
+    }
 };
 
+/**
+ * Returns 400 if arguments are missing
+ * Returns 500 if there was an error processing the request
+ * @param {Express request} req
+ * @param {Express response} res
+ */
 exports.getPostcodes = async function(req, res) {
-    data = await apiService.getPostcodes(req.params.lat, req.params.long);
-    res.send(JSON.stringify(data, null, 2));
+    if (req.params.lat == undefined || req.params.long == undefined) {
+        res.status(400);
+        res.send("Lat or long arguments missing");
+    } else {
+        try {
+            var data = await apiService.getPostcodes(
+                req.params.lat,
+                req.params.long
+            );
+            res.send(JSON.stringify(data, null, 2));
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log(err);
+            res.status(500);
+            res.send("Error getting postcodes");
+        }
+    }
 };
