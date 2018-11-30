789eb0caae73753f9bf3ffa0ec98adcf64cb5d94
unknown <fm263@sussex.ac.uk>
2018-11-30 21:19:13 +0000

    more tests

--- ASE_backend/test/test-postcode.js
+++ ASE_backend/test/test-postcode.js
@@ -1,5 +1,4 @@
 const postcodeService =  require('../services/postcodeService.js');
-var nock = require('nock');
 var expect  = require('chai').expect;
 var request = require('request');
 
@@ -12,15 +11,28 @@ describe('Postcode details', function(){
        expect(details.Longitude).to.equal('-0.132927')
     });
   });
+
+  it('returns undefined', function(){
+    postcodeService.getPostcodeDetails('0009011').then(details => {
+      expect(details.Latitude).to.equal(undefined);
+      expect(details.Longitude).to.equal(undefined)
+    });
+  });
+
 });
 
-
 describe('Postcodes near around', function(){
-  it('returns postcodes within an area', function() {
+  it('does not find any postcodes', function() {
   postcodeService.getPostcodesNearAround('50.845112','-0.132927').then(details => {
     expect(Array.isArray(details)).to.equal(true);
     expect(details).to.have.length.above(1);
+  });
+  });
 
+  it('returns postcodes within an area', function() {
+  postcodeService.getPostcodesNearAround('0','0').then(details => {
+    expect(Array.isArray(details)).to.equal(true);
+    expect(details.length).to.equal(0);
   });
   });
 });
