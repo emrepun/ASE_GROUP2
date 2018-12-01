36934fffe2dd961e6d8272a6cb5751a6dca41749
Rohan Bhayana <rallade@hotmail.com>
2018-12-01 19:51:57 +0000

    Added new Mongo Models for resolution scales

---
+++ ASE_backend/models/sector.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+//MongoDB Person schema file, used to access postcodeDatas
+
+const mongoose = require("mongoose");
+require("mongoose-double")(mongoose);
+
+var SchemaTypes = mongoose.Schema.Types;
+
+var postcode = mongoose.model("sector", {
+    _id: { type: String, trim: true },
+    Latitude: { type: SchemaTypes.Double },
+    Longitude: { type: SchemaTypes.Double },
+    Average_price: { type: SchemaTypes.Double }
+});
+
+module.exports = { sector: postcode };
